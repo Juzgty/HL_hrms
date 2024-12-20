0fbf7575370c4c3ef8e93f96381243c437b40b6c
Your Name <you@example.com>
2024-12-20 16:05:15 +0800

    feat(controller): 添加登录和注册页面的路由映射
    - 在AdminController中添加了处理登录和注册请求的映射方法
    - 更新了login.html和register.html模板中的表单动作和链接地址
    -优化了页面跳转的逻辑，去除了不必要的.html后缀

--- src/main/java/cn/hl/hlhrms/controller/AdminController.java
+++ src/main/java/cn/hl/hlhrms/controller/AdminController.java
@@ -1,8 +1,16 @@
 package cn.hl.hlhrms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
-
+    @GetMapping ("/login")
+    public String showLoginPage() {
+        return "login"; // 返回 login.html 模板
+    }
+    @GetMapping("/register")
+    public String showRegisterPage() {
+        return "register"; // 返回 register.html 模板
+    }
 }
